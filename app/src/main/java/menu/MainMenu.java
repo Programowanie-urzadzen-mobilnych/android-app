7110f849657361c8e6e9385117e4ebb0b69536b4
Legut <wlegutowski@gmail.com>
2020-05-08 14:53:28 +0200

    SR-84 Base menu activity created.

---
+++ app/src/main/java/menu/MainMenu.java
@@ -0,0 +1,16 @@
+package menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.grupa33inf.ssi.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
--- app/src/main/java/pl/grupa33inf/ssi/MainActivity.java
+++ app/src/main/java/pl/grupa33inf/ssi/MainActivity.java
@@ -2,13 +2,19 @@ package pl.grupa33inf.ssi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.access_control.LoginMain;
+
+import measurements.Measurements;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		Intent i = new Intent(this, LoginMain.class);
+		startActivity(i);
 	}
 }
