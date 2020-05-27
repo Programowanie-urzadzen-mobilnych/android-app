f67086187a9f262ad3f4de870e047d9abf0e5523
Aksel <aksel.stankiewicz@onet.eu>
2020-05-27 09:26:59 +0200

    feature/SR-59

--- app/src/main/java/pl/grupa33inf/ssi/MainActivity.java
+++ app/src/main/java/pl/grupa33inf/ssi/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Date;
+
+import pl.grupa33inf.ssi.data_store.api.DataStoreApi;
+import pl.grupa33inf.ssi.data_store.api.EntryType;
+import pl.grupa33inf.ssi.data_store.api.ILogDatastore;
+import pl.grupa33inf.ssi.data_store.api.InfoLogEntry;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
